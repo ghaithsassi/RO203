319bb1f83e7234ee7fd71adc04faa0f1870a9284
algoExpert <ghaithsassi@github.com>
2020-05-02 16:42:57 +0200

    geration

---
+++ julia/src/tracks/generation.jl
@@ -0,0 +1,11 @@
+
+
+
+function generateInstance(n::Int64, m::Int64,density::Float64)
+
+    h = Array{Int64}(undef,m,1)
+    v = Array{Int64}(undef,n,1)
+
+    ad = Dict{Tuple{Int64},}(undef,m,1)
+    
+end 
